1aa35f7c40b1baf3f5c963f8c1a27ca485e38ce1
tom <442817485@qq.com>
2022-09-13 02:21:38 +0000

    modified:   User/Expand.py 	modified:   User/__pycache__/Expand.cpython-39.pyc

--- User/Expand.py
+++ User/Expand.py
@@ -3,13 +3,25 @@
 #To add your extension function just add it as a submenu of the main menu
 
 import tkinter as tk
+from tkinter.colorchooser import Chooser, askcolor
+from search_enlish.Search import History_Search,main
 
 class Extension:
     def __init__(self,) -> None:
         self.mainmenu.add_command(label="check connect", command=self.check_connect)
+        self.mainmenu.add_command(label="color chooser", command=self.colorchoose)
+        self.mainmenu.add_command(
+            label="Search", command=self.Search)
         pass
 
     def check_connect(self):
         self.output(
             ("与服务器断开连接!" if self.mess.server_is_start == 0 else "连接至服务器"))
     
+    def colorchoose(self):
+        color=askcolor()
+        self.output(color)
+    
+    def Search(self):
+        main()
+        pass
